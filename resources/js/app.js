966d41b72a5553816f98d6f1056ed1e155d88e16
Sergio Díaz <codediaz@outlook.es>
2022-03-19 07:45:23 -0500

    Jetstream workspace and configuration settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,20 @@
-/*require('./bootstrap');
+require('./bootstrap');
 
-import Vue from 'vue'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
-import App from './vue/app'
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-const app = new Vue({
-    el:'#app',
-    components:{App}
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
 });
-*/
+
+InertiaProgress.init({ color: '#4B5563' });

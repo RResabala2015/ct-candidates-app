fa318dc6d7ac73836ceed342be4367f06346b7c7
Sergio Díaz <codediaz@outlook.es>
2022-03-18 21:36:23 -0500

    .env added, charset and collation added database.php and schema:defaultString added in AppServiceProcider.php

--- config/database.php
+++ config/database.php
@@ -57,6 +57,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
